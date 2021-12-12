3bd1e6147c4f0c1f2a793bee3c946df25e84f01b
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2021-12-13 00:07:05 +0100

    Update keys.js

--- 2122_CMP5_YipD_Fitbit_activiteit/companion/keys.js
+++ 2122_CMP5_YipD_Fitbit_activiteit/companion/keys.js
@@ -1,4 +1,3 @@
-export const API_KEY =
-  'pk.eyJ1IjoiZGFubnl5aXA5NyIsImEiOiJja2kxNDkzZG82bXV2MzVsNm84Y2R4emY4In0.8otJObef7or-Qx-I3-hf9g';
+export const API_KEY = 'hier komt uw key';
 
-export const apikey = '0ccde133820becbc1c00c4fdb9606118';
+export const apikey = 'key';

aa9ab9889d4d8b7b88a78abd7e8decb8aed9a0a8
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2021-12-12 23:41:31 +0100

    Create keys.js

---
+++ 2122_CMP5_YipD_Fitbit_activiteit/companion/keys.js
@@ -0,0 +1,4 @@
+export const API_KEY =
+  'pk.eyJ1IjoiZGFubnl5aXA5NyIsImEiOiJja2kxNDkzZG82bXV2MzVsNm84Y2R4emY4In0.8otJObef7or-Qx-I3-hf9g';
+
+export const apikey = '0ccde133820becbc1c00c4fdb9606118';

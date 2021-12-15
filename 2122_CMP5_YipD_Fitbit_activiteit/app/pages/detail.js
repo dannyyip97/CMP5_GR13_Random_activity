5c7d8fbbb8271d05234a658bb77f591832f7be07
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2021-12-15 10:55:06 +0100

    Update detail.js

--- 2122_CMP5_YipD_Fitbit_activiteit/app/pages/detail.js
+++ 2122_CMP5_YipD_Fitbit_activiteit/app/pages/detail.js
@@ -1,7 +1,15 @@
 import document from 'document';
 import { getLocationName } from '../commands';
-import { getStateItem, setStateCallback, removeStateCallback } from '../state';
+import { switchPage } from '../navigation';
+import {
+  getStateItem,
+  setStateCallback,
+  removeStateCallback,
+  setStateItem,
+} from '../state';
+import { getListData } from '../commands';
 
+let myList = null;
 let $button = null;
 let $locationName = null;
 
@@ -10,6 +18,33 @@ function doSomething() {
 }
 
 function draw() {
+  const list = getStateItem('listData');
+
+  console.log(JSON.stringify(list));
+
+  myList.delegate = {
+    getTileInfo: (index) => {
+      return {
+        type: 'my-pool',
+        value: list[index],
+        index: index,
+      };
+    },
+    configureTile: (tile, info) => {
+      console.log(`Item: ${info.index}`);
+      if (info.type == 'my-pool') {
+        tile.getElementById('text').text = `${info.value.name}`;
+        let touch = tile.getElementById('touch');
+        touch.onclick = function () {
+          setStateItem('detailId', info.value.id);
+          switchPage('detail');
+        };
+      }
+    },
+  };
+
+  // length must be set AFTER delegate
+  myList.length = list.length;
   $locationName.text = getStateItem('location');
 }
 
@@ -17,6 +52,9 @@ export function destroy() {
   console.log('destroy detail page');
   $locationName = null;
   $button = null;
+  myList = null;
+  removeStateCallback('replace');
+  removeStateCallback('index', draw);
   removeStateCallback('detail');
 }
 
@@ -27,10 +65,14 @@ export function init() {
   $button.onclick = () => {
     destroy();
     document.history.back();
+    switchPage('index');
   };
-
+  myList = document.getElemenetById('myList');
+  getListData();
   doSomething();
   getLocationName();
+  setStateCallback('index', draw);
+  setStateCallback('replace', draw);
   setStateCallback('detail', draw);
   // draw();
 }

0625449b69769da1fc176362ec70eafe60b32670
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2021-12-15 10:54:46 +0100

    Create data.js

---
+++ 2122_CMP5_YipD_Fitbit_activiteit/companion/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    name: 'Plan a vacation you have always wanted to take',
+    value: 'Test-1',
+    user: 'Danny-1',
+    id: 'zomer',
+  },
+  {
+    name: 'Hold a video game tournament with some friends',
+    value: 'Test-2',
+    user: 'Danny-2',
+    id: 'winter',
+  },
+  {
+    name: 'Teach your dog a new trick',
+    value: 'Test-3',
+    user: 'Danny-3',
+    id: 'herfst',
+  },
+  {
+    name: 'Take a spontaneous road trip with some friends',
+    value: 'Test-4',
+    user: 'Danny-4',
+    id: 'lente',
+  },
+  {
+    name: 'Have a picnic with some friends',
+    value: 'Test-5',
+    user: 'Danny-5',
+    id: 'zomer',
+  },
+];

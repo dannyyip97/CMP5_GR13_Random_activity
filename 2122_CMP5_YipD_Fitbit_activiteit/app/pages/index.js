b29983600dc37d890bed52f98f1fdfd0c45d674b
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2021-12-15 10:55:03 +0100

    Update index.js

--- 2122_CMP5_YipD_Fitbit_activiteit/app/pages/index.js
+++ 2122_CMP5_YipD_Fitbit_activiteit/app/pages/index.js
@@ -2,18 +2,30 @@ import document from 'document';
 import { switchPage } from '../navigation';
 import zeroPad from '../utils/zero-pad';
 import clock from 'clock';
+import { getStateItem } from '../state';
 import { preferences } from 'user-settings';
+
 //import { init as getStateItem } from '../state';
 
-/*const $antwoorden = ["Ja", "Neen", "Ja zeker", "Alles kan nog","Jammer maar helaas", "Euhm wil je het echt weten", "Wat een grap"];
-const $activiteit = document.getElementById("#text");
-const $knop = document.querySelector(".Submit");*/
-//const $weather = document.getElementById('weather');
+/*const $antwoorden = [
+  'Ja',
+  'Neen',
+  'Ja zeker',
+  'Alles kan nog',
+  'Jammer maar helaas',
+  'Euhm wil je het echt weten',
+  'Wat een grap',
+];*/
 
+//const $weather = document.getElementById('weather');
 let $buttonDetail = null;
 let $buttonReplace = null;
 let time = '';
 let $time = null;
+
+let $weather = null;
+
+/*let $activiteit = '';*/
 
 function doSomething() {
   console.log('hallo index');
@@ -24,6 +36,7 @@ export function destroy() {
   $buttonDetail = null;
   $buttonReplace = null;
   $time = null;
+  $weather = null;
 }
 
 export function init() {
@@ -31,6 +44,10 @@ export function init() {
   $buttonDetail = document.getElementById('detail-button');
   $buttonReplace = document.getElementById('replace-button');
   $time = document.getElementById('time');
+  $weather = document.getElementById('weather');
+  /*$activiteit = document.getElementById('activiteit');*/
+
+  /*$activiteit = document.getElementById('activiteit');*/
 
   $buttonDetail.onclick = () => {
     switchPage('detail', true);
@@ -48,10 +65,27 @@ export function init() {
 }
 
 function draw() {
-  if ($time) {
+  if (($time, $weather)) {
+    $weather.text = getStateItem('weather');
     $time.text = time;
   }
 }
+
+//activiteiten
+
+/*function willekeurigGetal(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function answer() {
+  if ($activiteit.onclick) {
+    document.getElementById('activiteit').text =
+      $antwoorden[willekeurigGetal(0, 6)];
+  }
+}
+$activiteit.addEventListener('click', answer);*/
 
 function updateTime(datetime) {
   const minute = datetime.getMinutes();
@@ -73,23 +107,4 @@ function updateTime(datetime) {
 
 // draw when code loaded
 
-//activiteiten
-/* $activiteit.appendChild($antwoorden)
-function willekeurigGetal(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function answer(){
-  if ($knop.onclick = () => {
-    document.getElementById("activiteit").innerHTML = //$antwoorden[willekeurigGetal(0,6)];
-  }
-}
-$knop.addEventListener("click",answer);*/
 // draw
-
-/*function draw() {
-  $weather.text = getStateItem('weather');
-}
-draw();*/

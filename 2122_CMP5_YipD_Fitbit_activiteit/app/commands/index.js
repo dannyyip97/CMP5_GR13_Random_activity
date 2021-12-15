d8dba11b61476373da31b04654812d8974a4301f
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2021-12-15 10:55:08 +0100

    Update index.js

--- 2122_CMP5_YipD_Fitbit_activiteit/app/commands/index.js
+++ 2122_CMP5_YipD_Fitbit_activiteit/app/commands/index.js
@@ -9,9 +9,27 @@ export function getLocationName() {
   }
 }
 
+// get item
+export function getListItem(id) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send({
+      command: 'getListItem',
+      id: id,
+    });
+  }
+}
+// get data
+export function getListData() {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send({
+      command: 'getListData',
+    });
+  }
+}
 // set up
 export function init() {
   messaging.peerSocket.addEventListener('open', () => {
     getLocationName();
+    getListData();
   });
 }

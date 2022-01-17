5bca2c1f4879305f665a856fbd1617233189b473
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2022-01-17 04:42:07 +0100

    Update data.js

--- 2122_CMP5_YipD_Fitbit_activiteit/companion/data.js
+++ 2122_CMP5_YipD_Fitbit_activiteit/companion/data.js
@@ -1,3 +1,5 @@
+import { gettext } from 'i18n';
+
 export const data = [
   {
     name: 'Plan a vacation you have always wanted to take',
@@ -29,4 +31,101 @@ export const data = [
     user: 'Danny-5',
     id: 'zomer',
   },
+  {
+    name: 'Create your personal website',
+    value: 'Test-6',
+    user: 'Danny-6',
+    id: 'winter',
+  },
+  {
+    name: 'Repaint a room in your house',
+    value: 'Test-7',
+    user: 'Danny-7',
+    id: 'winter',
+  },
+  {
+    name: 'Bake pastries for/with you and your partner',
+    value: 'Test-8',
+    user: 'Danny-8',
+    id: 'winter',
+  },
+  {
+    name: 'Take a hike at a local park',
+    value: 'Test-9',
+    user: 'Danny-9',
+    id: 'zomer',
+  },
+  {
+    name: 'Go on a fishing trip with some friends',
+    value: 'Test-10',
+    user: 'Danny-10',
+    id: 'zomer',
+  },
+  {
+    name: 'Go stargazing',
+    value: 'Test-11',
+    user: 'Danny-11',
+    id: 'zomer',
+  },
+  {
+    name: 'Watch a movie you would never usually watch',
+    value: 'Test-12',
+    user: 'Danny-12',
+    id: 'herfst',
+  },
+  {
+    name: 'Start reading a book',
+    value: 'Test-13',
+    user: 'Danny-13',
+    id: 'herfst',
+  },
+  {
+    name: 'Pull a harmless prank on one of your friends ',
+    value: 'Test-14',
+    user: 'Danny-14',
+    id: 'herfst',
+  },
+  {
+    name: 'Take an online cooking class',
+    value: 'Test-15',
+    user: 'Danny-15',
+    id: 'herfst',
+  },
+  {
+    name: 'Play some card games ',
+    value: 'Test-16',
+    user: 'Danny-16',
+    id: 'winter',
+  },
+  {
+    name: 'Run 5km in your local park',
+    value: 'Test-17',
+    user: 'Danny-17',
+    id: 'zomer',
+  },
+  {
+    name: 'Draw one of your favorite disney character',
+    value: 'Test-18',
+    user: 'Danny-18',
+    id: 'herfst',
+  },
+  {
+    name: 'Learn something new on Youtube',
+    value: 'Test-19',
+    user: 'Danny-19',
+    id: 'winter',
+  },
+  {
+    name: 'Take nice pictures with some friends and post it',
+    value: 'Test-20',
+    user: 'Danny-20',
+    id: 'zomer',
+  },
+  {
+    name: 'Make a sandcastle',
+    value: 'Test-21',
+    user: 'Danny-21',
+    id: 'zomer',
+  },
 ];
+console.log(gettext('hello'));

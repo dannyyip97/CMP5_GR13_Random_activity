8bec6559bf46c7fe1ff517a4cb62bee9dc04e0e1
Michiel-ux <80071823+Michiel-ux@users.noreply.github.com>
2022-01-17 12:37:48 +0100

    Update data.js

--- 2122_CMP5_YipD_Fitbit_activiteit/companion/data.js
+++ 2122_CMP5_YipD_Fitbit_activiteit/companion/data.js
@@ -8,19 +8,19 @@ export const data = [
     id: 'zomer',
   },
   {
-    name: 'Hold a video game tournament with some friends',
+    name: gettext('Hold a video game tournament with some friends'),
     value: 'Test-2',
     user: 'Danny-2',
     id: 'winter',
   },
   {
-    name: 'Teach your dog a new trick',
+    name: gettext('Teach your dog a new trick'),
     value: 'Test-3',
     user: 'Danny-3',
     id: 'herfst',
   },
   {
-    name: 'Take a spontaneous road trip with some friends',
+    name: gettext('Take a spontaneous road trip with some friends'),
     value: 'Test-4',
     user: 'Danny-4',
     id: 'lente',

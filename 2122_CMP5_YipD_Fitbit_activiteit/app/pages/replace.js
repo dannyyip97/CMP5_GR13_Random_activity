8555fa9c1c070e674bb195c1166b062fb4360bbe
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2022-01-17 04:41:53 +0100

    Update replace.js

--- 2122_CMP5_YipD_Fitbit_activiteit/app/pages/replace.js
+++ 2122_CMP5_YipD_Fitbit_activiteit/app/pages/replace.js
@@ -1,31 +1,17 @@
 import document from 'document';
 import { switchPage } from '../navigation';
-import { getStateItem, setStateCallback, removeStateCallback } from '../state';
+import { setStateCallback, removeStateCallback } from '../state';
 let $button = null;
-let $letter = null;
-let $timestamp = null;
 
 function doSomething() {
   console.log('hallo replace');
 }
 
-function draw() {
-  const letter = getStateItem('letter');
-  const timestamp = getStateItem('companionTimestamp');
-  $timestamp.text = timestamp;
-
-  if (letter) {
-    $letter.text = letter;
-  } else {
-    $letter.text = 'set letter';
-  }
-}
+function draw() {}
 
 export function destroy() {
   console.log('destroy replace page');
   $button = null;
-  $letter = null;
-  $timestamp = null;
 
   removeStateCallback('replace');
   removeStateCallback('index', draw);
@@ -33,8 +19,7 @@ export function destroy() {
 
 export function init() {
   console.log('init replace page');
-  $letter = document.getElementById('letter');
-  $timestamp = document.getElementById('timestamp');
+
   $button = document.getElementById('back-button');
   $button.onclick = () => {
     destroy();

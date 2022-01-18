7121c88201eb04b2e352848e9c37528839a0dd2e
dannyyip97 <92430502+dannyyip97@users.noreply.github.com>
2022-01-18 12:31:44 +0100

    Create data.js

---
+++ 2122_CMP5_YipD_Fitbit_activiteit/app/pages/data.js
@@ -0,0 +1,131 @@
+import { gettext } from 'i18n';
+
+export const data = [
+  {
+    name: gettext('Plan a vacation you have always wanted to take'),
+    value: 'Test-1',
+    user: 'Danny-1',
+    id: 'zomer',
+  },
+  {
+    name: gettext('Hold a video game tournament with some friends'),
+    value: 'Test-2',
+    user: 'Danny-2',
+    id: 'winter',
+  },
+  {
+    name: gettext('Teach your dog a new trick'),
+    value: 'Test-3',
+    user: 'Danny-3',
+    id: 'herfst',
+  },
+  {
+    name: gettext('Take a spontaneous road trip with some friends'),
+    value: 'Test-4',
+    user: 'Danny-4',
+    id: 'lente',
+  },
+  {
+    name: gettext('Have a picnic with some friends'),
+    value: 'Test-5',
+    user: 'Danny-5',
+    id: 'zomer',
+  },
+  {
+    name: gettext('Create your personal website'),
+    value: 'Test-6',
+    user: 'Danny-6',
+    id: 'winter',
+  },
+  {
+    name: gettext('Repaint a room in your house'),
+    value: 'Test-7',
+    user: 'Danny-7',
+    id: 'winter',
+  },
+  {
+    name: gettext('Bake pastries for/with you and your partner'),
+    value: 'Test-8',
+    user: 'Danny-8',
+    id: 'winter',
+  },
+  {
+    name: gettext('Take a hike at a local park'),
+    value: 'Test-9',
+    user: 'Danny-9',
+    id: 'zomer',
+  },
+  {
+    name: gettext('Go on a fishing trip with some friends'),
+    value: 'Test-10',
+    user: 'Danny-10',
+    id: 'zomer',
+  },
+  {
+    name: gettext('Go stargazing'),
+    value: 'Test-11',
+    user: 'Danny-11',
+    id: 'zomer',
+  },
+  {
+    name: gettext('Watch a movie you would never usually watch'),
+    value: 'Test-12',
+    user: 'Danny-12',
+    id: 'herfst',
+  },
+  {
+    name: gettext('Start reading a book'),
+    value: 'Test-13',
+    user: 'Danny-13',
+    id: 'herfst',
+  },
+  {
+    name: gettext('Pull a harmless prank on one of your friends'),
+    value: 'Test-14',
+    user: 'Danny-14',
+    id: 'herfst',
+  },
+  {
+    name: gettext('Take an online cooking class'),
+    value: 'Test-15',
+    user: 'Danny-15',
+    id: 'herfst',
+  },
+  {
+    name: gettext('Play some card games'),
+    value: 'Test-16',
+    user: 'Danny-16',
+    id: 'winter',
+  },
+  {
+    name: gettext('Run 5km in your local park'),
+    value: 'Test-17',
+    user: 'Danny-17',
+    id: 'zomer',
+  },
+  {
+    name: gettext('Draw one of your favorite disney characters'),
+    value: 'Test-18',
+    user: 'Danny-18',
+    id: 'herfst',
+  },
+  {
+    name: gettext('Learn something new on Youtube'),
+    value: 'Test-19',
+    user: 'Danny-19',
+    id: 'winter',
+  },
+  {
+    name: gettext('Take nice pictures with some friends and post it'),
+    value: 'Test-20',
+    user: 'Danny-20',
+    id: 'zomer',
+  },
+  {
+    name: gettext('Make a sandcastle'),
+    value: 'Test-21',
+    user: 'Danny-21',
+    id: 'zomer',
+  },
+];
+console.log(gettext('hello'));
